550dee13f7931b89bc19074de40c89a3817a973b
Seriki yakub <sandraisom00@gmail.com>
2026-01-07 00:33:24 +0100

    Add files via upload

---
+++ KRB5/Keyblock.c
@@ -0,0 +1,3 @@
+krb5_error_code krb5_auth_con_getlocalsubkey(krb5_context context,
+                                              krb5_auth_context auth_context,
+                                              krb5_keyblock **keyblock)
